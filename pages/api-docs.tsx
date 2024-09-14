c366eac953d44496d5d3e5523f87e43e00597bf5
cccwon2 <cccwon2@naver.com>
2024-09-14 13:23:34 +0900

    chore: SwaggerUI 관련 설정 추가

--- pages/api-docs.tsx
+++ pages/api-docs.tsx
@@ -1,5 +1,12 @@
+// pages/api-docs.tsx
 import React from "react";
-import SwaggerUI from "swagger-ui-react";
+import dynamic from "next/dynamic";
+
+// 서버 사이드 렌더링을 비활성화하고 동적으로 SwaggerUI를 로드합니다.
+const SwaggerUI = dynamic(() => import("swagger-ui-react"), { ssr: false });
+
+// Swagger UI 스타일을 가져옵니다.
+import "swagger-ui-react/swagger-ui.css";
 
 const ApiDocs: React.FC = () => {
   return <SwaggerUI url="/api/swagger" />;
