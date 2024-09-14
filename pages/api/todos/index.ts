b37906a359f53c3ae2fdb334d0dbb02bac19391e
cccwon2 <cccwon2@naver.com>
2024-09-14 13:59:46 +0900

    chore: swagger 스키마 정의 추가

--- pages/api/todos/index.ts
+++ pages/api/todos/index.ts
@@ -3,6 +3,33 @@ import { PrismaClient } from "@prisma/client";
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Todo:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: "Todo 항목에 대한 고유 식별자"
+ *         title:
+ *           type: string
+ *           description: "Todo 항목의 제목"
+ *         description:
+ *           type: string
+ *           description: "Todo 항목에 대한 상세 설명"
+ *         completed:
+ *           type: boolean
+ *           description: "Todo가 완료되었는지 여부"
+ *     TodoInput:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: "생성할 Todo 항목의 제목"
+ *         description:
+ *           type: string
+ *           description: "생성할 Todo 항목에 대한 상세 설명"
+ *
  * /api/todos:
  *   get:
  *     summary: "Todo 리스트 조회"
--- swagger.js
+++ swagger.js
@@ -1,4 +1,3 @@
-// swagger.js
 import swaggerJsdoc from "swagger-jsdoc";
 
 const options = {
@@ -8,10 +7,34 @@ const options = {
       title: "Todo API",
       version: "1.0.0",
     },
+    components: {
+      schemas: {
+        Todo: {
+          type: "object",
+          properties: {
+            id: {
+              type: "integer",
+              description: "The unique identifier for a todo item",
+            },
+            title: {
+              type: "string",
+              description: "The title of the todo item",
+            },
+            description: {
+              type: "string",
+              description: "Detailed description of the todo",
+            },
+            completed: {
+              type: "boolean",
+              description: "Whether the todo is completed",
+            },
+          },
+        },
+      },
+    },
   },
-  apis: ["./pages/api/**/*.ts"],
+  apis: ["./pages/api/**/*.ts"], // API 경로
 };
 
 const swaggerSpec = swaggerJsdoc(options);
-
 export default swaggerSpec;
