186e473816ee30ab421d2b4b4519fd928119a6f0
cccwon2 <cccwon2@naver.com>
2024-09-16 21:49:41 +0900

    chore: 쿠키 옵션 설정 브라우저 닫힐때 초기화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
-import { supabase } from '@/lib/initSupabase'
-import '@/styles/app.css'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import type { AppProps } from 'next/app'
+import { supabase } from "@/lib/initSupabase";
+import "@/styles/app.css";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [supabaseClient] = useState(() =>
+    createPagesBrowserClient({
+      cookieOptions: {
+        name: "sb-session",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+        domain: process.env.NEXT_PUBLIC_DOMAIN || undefined,
+        path: "/",
+      },
+    })
+  );
+
   return (
-    <SessionContextProvider supabaseClient={supabase}>
+    <SessionContextProvider supabaseClient={supabaseClient}>
       <Component {...pageProps} />
     </SessionContextProvider>
-  )
+  );
 }
