75c15c30ccb08d60a35aef827fbfdfdaa3410461
cccwon2 <cccwon2@naver.com>
2024-09-17 01:19:51 +0900

    chore: 회원가입, 로그인 타이틀 순서 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             <div className="w-full h-full flex justify-center items-center p-4">
               <div className="w-full h-full sm:h-auto sm:w-2/5 max-w-sm p-5 bg-white shadow flex flex-col text-base">
                 <span className="font-sans text-4xl text-center pb-2 mb-1 border-b mx-4 align-center">
-                  {authState === "login" ? "로그인" : "회원가입"}
+                  {authState === "signup" ? "회원가입" : "로그인"}
                 </span>
                 <Auth
                   supabaseClient={supabase}
